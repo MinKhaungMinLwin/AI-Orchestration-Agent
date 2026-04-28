4be3935076bd3b78bd0d27d30230588efd386d7c
MinKhaungMinLwin <83025403+MinKhaungMinLwin@users.noreply.github.com>
2026-04-28 16:09:58 +0700

    new Openapi 2nd time update

--- app/tstation-ai/common/tstation_be_api_client/hkt_api_client/models/rcmd_type.py
+++ app/tstation-ai/common/tstation_be_api_client/hkt_api_client/models/rcmd_type.py
@@ -2,9 +2,25 @@ from enum import Enum
 
 
 class RcmdType(str, Enum):
+    ALL_WEATHER = "all_weather"
+    COMMUTE = "commute"
     DISCOUNT = "discount"
+    EV = "ev"
+    FAMILY = "family"
+    HANDLING = "handling"
+    HEAVY_LOAD = "heavy_load"
+    HIGH_SPEED = "high_speed"
+    LONG_DISTANCE = "long_distance"
+    LOW_VIBRATION = "low_vibration"
+    PERFORMANCE = "performance"
+    SAFE_KIDS = "safe_kids"
+    SNOW = "snow"
     TSTATION = "tstation"
+    URBAN = "urban"
     VALUE = "value"
+    WARRANTY = "warranty"
+    WEEKEND = "weekend"
+    WET = "wet"
 
     def __str__(self) -> str:
         return str(self.value)
